7148cc9e7fef9173ebe6b8898029491ca3fbe745
SijuEC <eapen.siju@gmail.com>
2023-03-07 11:01:14 +0200

    started out oop implementation, setup a funtion to handle input types that are not int type

--- whack_a_pixel.py
+++ whack_a_pixel.py
@@ -3,41 +3,53 @@ import time
 import random
 import sys
 
-set_brightness(0.1)
-life = 3
-level = 1
 
-while True:
-	pixel = random.randint(0,7)
-	set_pixel(pixel, 255, 255, 255)
-	show()
-	time.sleep(1/level)
-	#set_pixel(pixel, 0, 0, 0)
-	clear()
-	show()
-	user_pixel = int(input())
-	#time.sleep(0.5)
-	#user_pixel = pixel + 10
-	if user_pixel-1 == pixel:
-		set_all(255, 255, 255)
+def input_from_user():
+	user_pixel = input()
+	try:
+		user_pixel = int(user_pixel)
+	except ValueError:
+		print(f"You did not enter an integer")
+		return(10)
+	return(user_pixel)
+
+
+def main():
+	set_brightness(0.1)
+	life = 3
+	level = 1
+
+	while True:
+		pixel = random.randint(0,7)
+		set_pixel(pixel, 255, 255, 255)
 		show()
-		time.sleep(0.2)
-		set_all(0, 0, 0)
-		#clear()
+		time.sleep(1/level)
+		clear()
 		show()
-		time.sleep(0.5)
-		f"Correct. You still have {life} lives left."
-	else:
-		for i in range(life):
-			set_all(255, 0, 0)
+		user_pixel = input_from_user()
+		if user_pixel-1 == pixel:
+			set_all(255, 255, 255)
 			show()
 			time.sleep(0.2)
 			set_all(0, 0, 0)
 			#clear()
 			show()
 			time.sleep(0.5)
-		if life == 0:
-			sys.exit(f"***Game Over***")
-		life -= 1
-		f"You have {life} lives left."
-	level += 1
+			print(f"Correct. You still have {life} lives left.")
+		else:
+			for i in range(life):
+				set_all(255, 0, 0)
+				show()
+				time.sleep(0.2)
+				set_all(0, 0, 0)
+				#clear()
+				show()
+				time.sleep(0.5)
+			if life == 0:
+				sys.exit(f"***Game Over***")
+			life -= 1
+			print(f"You have {life} lives left.")
+		level += 1
+
+if __name__ == "__main__":
+	main()
